98d4b9f7210301df5dff5615c3d91961109425dc
madpotato1713 <42724769+madpotato1713@users.noreply.github.com>
2021-09-21 18:36:40 +0900

    상속 & 믹스인

--- app.ts
+++ app.ts
@@ -28,7 +28,6 @@ interface NewsComment extends News {
 }
 
 const container: HTMLElement | null = document.getElementById('root');
-const ajax: XMLHttpRequest = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
@@ -37,12 +36,47 @@ const store: Store = {
     feeds: [],
 };
 
-function getData<AjaxResponse>(url: string): AjaxResponse {
-    ajax.open('GET', url, false); // false: 동기로 가져옴
-    ajax.send();
+function applyApiMixins(targetClass: any, baseClasses: any[]): void {
+    baseClasses.forEach((baseClass) => {
+        Object.getOwnPropertyNames(baseClass.prototype).forEach((name) => {
+            const descriptor = Object.getOwnPropertyDescriptor(
+                baseClass.prototype,
+                name
+            );
 
-    return JSON.parse(ajax.response);
+            if (descriptor) {
+                Object.defineProperty(targetClass.prototype, name, descriptor);
+            }
+        });
+    });
 }
+
+class Api {
+    getRequest<AjaxResponse>(url: string): AjaxResponse {
+        const ajax = new XMLHttpRequest();
+        ajax.open('GET', url, false);
+        ajax.send();
+
+        return JSON.parse(ajax.response);
+    }
+}
+
+class NewsFeedApi {
+    getData(): NewsFeed[] {
+        return this.getRequest<NewsFeed[]>(NEWS_URL);
+    }
+}
+
+class NewsDetailApi {
+    getData(id: string): NewsDetail {
+        return this.getRequest<NewsDetail>(CONTENT_URL.replace('@id', id));
+    }
+}
+
+interface NewsFeedApi extends Api {}
+interface NewsDetailApi extends Api {}
+applyApiMixins(NewsFeedApi, [Api]);
+applyApiMixins(NewsDetailApi, [Api]);
 
 function makeFeeds(feeds: NewsFeed[]): NewsFeed[] {
     for (let i = 0; i < feeds.length; i++) {
@@ -61,6 +95,7 @@ function updateView(html: string): void {
 }
 
 function newsFeed(): void {
+    const api = new NewsFeedApi();
     let newsFeed: NewsFeed[] = store.feeds;
     const newsList = [];
     let template = `
@@ -89,7 +124,7 @@ function newsFeed(): void {
     `;
 
     if (newsFeed.length === 0) {
-        newsFeed = store.feeds = makeFeeds(getData<NewsFeed[]>(NEWS_URL));
+        newsFeed = store.feeds = makeFeeds(api.getData());
     }
 
     for (
@@ -149,7 +184,8 @@ function newsFeed(): void {
 
 function newsDetail(): void {
     const id = location.hash.substr(7);
-    const newsContent = getData<NewsDetail>(CONTENT_URL.replace('@id', id));
+    const api = new NewsDetailApi();
+    const newsContent = api.getData(id);
     let template = `
     <div class="bg-gray-600 min-h-screen pb-8">
       <div class="bg-white text-xl">
