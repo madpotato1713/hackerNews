46622cf551e3da44643c8b722dbfbdb7416fa427
madpotato1713 <42724769+madpotato1713@users.noreply.github.com>
2021-09-22 10:43:36 +0900

    gitignore 업데이트

--- dist/app.5cec07dd.js
+++ dist/app.5cec07dd.js
@@ -744,7 +744,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "13339" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "9779" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
